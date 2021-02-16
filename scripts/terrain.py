0edcc173c1940473bcd94da8b17290ded548c608
Lisa <you@example.com>
2021-02-16 16:53:32 -0500

    not working occupancy map add - make object oriented for subscriber

--- scripts/terrain.py
+++ scripts/terrain.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # license removed for brevity
 import rospy
-from std_msgs.msg import String
-from nav_msgs.msg import GridCells
+from nav_msgs.msg import GridCells, OccupancyGrid
 from geometry_msgs.msg import Point
 
 # randomly shuffle a sequence
@@ -11,54 +10,7 @@ from random import shuffle
 from random import choice
 
 
-def terrain():
-    pub_obs = rospy.Publisher('obstacle', GridCells, queue_size=10)
-    pub_clr = rospy.Publisher('clear_space', GridCells, queue_size=10)
-    pub_path = rospy.Publisher('path', GridCells, queue_size=10)
-    rospy.init_node('terrain', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-
-    path_len = 1
-    obstacles = point_gen(5, 5, 70, path_len)
-    clear_space = point_gen(5, 5, 40, path_len)
-    path_points = path_gen(path_len)
-
-    time_up = rospy.get_time() + 2
-    while not rospy.is_shutdown():
-        if rospy.get_time() > time_up:
-            if path_len < 20:
-                path_len += 1
-            obstacles = point_gen(5, 5, 100, path_len)
-            clear_space = point_gen(5, 5, 50, path_len)
-            path_points = path_gen(path_len)
-            time_up = rospy.get_time() + 1
-        # setup obstacles
-        obs = GridCells()
-        obs.cell_width = 1
-        obs.cell_height = 1
-        obs.header.frame_id = 'map'
-        obs.cells = obstacles
-        rospy.loginfo(str(obs))
-        # setup clear space
-        clr = GridCells()
-        clr.cell_width = 1
-        clr.cell_height = 1
-        clr.header.frame_id = 'map'
-        clr.cells = clear_space
-        # setup fake path
-        path = GridCells()
-        path.cell_width = 1
-        path.cell_height = 1
-        path.header.frame_id = 'map'
-        path.cells = path_points
-        # publish all
-        pub_obs.publish(obs)
-        pub_clr.publish(clr)
-        pub_path.publish(path)
-        rate.sleep()
-
-
-# generates some random points, can switch out for actual info
+# takes in occupancy grid, spits out lists of different types of cells
 def point_gen(x_max, y_max, n, see):
     points = []
     # seed random number generator
@@ -92,6 +44,69 @@ def path_gen(length):
         elif i % 2 == 0:
             start = [start[0] - 1, start[1]]
     return points
+
+
+def callback(data):
+    occupancy = data
+    rospy.loginfo(data.data)
+
+
+def terrain():
+    rospy.init_node('terrain', anonymous=True)
+    rate = rospy.Rate(10)  # 10hz
+
+    occupancy = []
+    sub_grid = rospy.Subscriber('fake_occupancy_grid', OccupancyGrid, callback, occupancy)
+    rospy.loginfo(str(occupancy))
+
+    pub_obs = rospy.Publisher('obstacle', GridCells, queue_size=10)
+    pub_clr = rospy.Publisher('clear_space', GridCells, queue_size=10)
+    pub_path = rospy.Publisher('path', GridCells, queue_size=10)
+
+
+
+    path_len = 1
+    obstacles = point_gen(5, 5, 70, path_len)
+    clear_space = point_gen(5, 5, 40, path_len)
+    path_points = path_gen(path_len)
+
+    time_up = rospy.get_time() + 2
+    while not rospy.is_shutdown():
+        rospy.loginfo(str(occupancy))
+
+        if rospy.get_time() > time_up:
+            if path_len < 20:
+                path_len += 1
+            obstacles = point_gen(5, 5, 100, path_len)
+            clear_space = point_gen(5, 5, 50, path_len)
+            path_points = path_gen(path_len)
+            time_up = rospy.get_time() + 1
+        # setup obstacles
+        obs = GridCells()
+        obs.cell_width = 1
+        obs.cell_height = 1
+        obs.header.frame_id = 'map'
+        obs.cells = obstacles
+        # rospy.loginfo(str(obs))
+        # setup clear space
+        clr = GridCells()
+        clr.cell_width = 1
+        clr.cell_height = 1
+        clr.header.frame_id = 'map'
+        clr.cells = clear_space
+        # setup fake path
+        path = GridCells()
+        path.cell_width = 1
+        path.cell_height = 1
+        path.header.frame_id = 'map'
+        path.cells = path_points
+        # publish all
+        pub_obs.publish(obs)
+        pub_clr.publish(clr)
+        pub_path.publish(path)
+        rate.sleep()
+
+
 
 
 
---
+++ scripts/test_occupancy_grid.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+
+from nav_msgs.msg import OccupancyGrid
+from std_msgs.msg import String
+
+def talker():
+    pub = rospy.Publisher('fake_occupancy', OccupancyGrid, queue_size=10)
+    rospy.init_node('fake_occupancy', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    grid = OccupancyGrid()
+    # see if you can change metadata to calling on a parameter of rviz?
+    grid.info.height = 20
+    grid.info.width = 20
+    values = [-1] * (20*20)
+    # do some stuff to values here
+    grid.data = values
+    while not rospy.is_shutdown():
+        pub.publish(grid)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
