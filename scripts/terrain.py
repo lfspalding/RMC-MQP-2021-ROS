ced5cd0ea36cadac0877334a821926784e8098e7
Lisa <you@example.com>
2021-04-05 15:57:17 -0400

    Astar broke, but added north/south padding

--- scripts/terrain.py
+++ scripts/terrain.py
@@ -63,32 +63,42 @@ class TerrainMap:
         return cells
 
     def path_gen(self):
-        start = (10, 0)
-        end = (3, 18)
-        maze = []
-        temp = []
+        start = (8, 0)  # starting position
+        end = (15, 18)  # TODO move this if it's on an obstacle
+        maze = []  # index of rows of the maze - built as they go
+        temp = []  # allows for construction of each row independent of size
+        holdover = []  # elements in the row that were obstacles
         for i in range(self.height):
-            for j in range(self.width):
-                temp.append(self.raw_map_data[i*self.width + j])
+            for j in range(self.width):  # Build row from the width of the grid input
+                temp.append(self.raw_map_data[i * self.width + j])
             row = [0] * len(temp)
-            for index, ele in enumerate(temp):
+            for ind in holdover:  # Add extra space for things that were obstacles last time
+                row[ind] = 1
+            holdover = []
+            for index, ele in enumerate(temp):  # parse row for obstacles
                 if ele > self.threshold:
                     row[index] = 1
+                    holdover.append(index)
                     try:
-                        row[index+1] = 1
-                        row[index-1] = 1
+                        row[index + 1] = 1
+                        row[index - 1] = 1
+                        maze[i - 1][index] = 1
                     except IndexError:
                         pass
             maze.append(row)
             temp = []
-        rospy.loginfo(maze)
-        tuple_path = astar(maze, start, end)
+        rospy.loginfo("\n" + str(maze[::-1]))
+        if len(maze) > 0:
+            tuple_path = astar(maze[::-1], start, end)
+        else:
+            tuple_path = [(0, 0)]
         path = make_points(tuple_path)
+        rospy.loginfo(tuple_path)
         return make_gridcells(path)
 
     def run(self):
         # path_len = 1
-        self.obstacles = self.sort_cells(40, 100) #these are probablility/possibly height values, check with John
+        self.obstacles = self.sort_cells(40, 100)  # these are probablility/possibly height values, check with John
         self.clear = self.sort_cells(0, 40)
         path = self.path_gen()
 
